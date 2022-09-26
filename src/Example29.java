5b641585a1b258f0e177f3fdf57a4c869dc72a17
rvt <alutoa2411@ieselcaminas.org>
2022-09-26 16:50:17 +0200

    Initial commit

---
+++ src/Example29.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Example29 {
+    public static void main(String[] args){
+
+        int num, div, i;
+
+        Scanner importValue;
+        importValue = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        num = importValue.nextInt();
+
+        while ()
+    }
+}
+
